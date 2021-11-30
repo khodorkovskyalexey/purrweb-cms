0729c0d78b3115c44123c36880df864304b4bfc9
terminator_79 <aezz120813@gmail.com>
2021-11-30 23:38:42 +0600

    feat: some crud params

--- src/playlists/playlists.controller.ts
+++ src/playlists/playlists.controller.ts
@@ -1,11 +1,32 @@
 import { Controller } from "@nestjs/common";
 import { Crud, CrudController } from "@nestjsx/crud";
+import { CreatePlaylistDto } from "./dtos/create-playlist.dto";
+import { UpdatePlaylistDto } from "./dtos/update-playlist.dto";
 import { Playlist } from "./entities/Playlist.entity";
 import { PlaylistsService } from "./playlists.service";
 
 @Crud({
     model: {
         type: Playlist,
+    },
+    dto: {
+        create: CreatePlaylistDto,
+        update: UpdatePlaylistDto,
+        replace: UpdatePlaylistDto,
+    },
+    params: {
+        playlist_id: {
+            field: 'id',
+            type: 'number',
+            primary: true,
+        },
+    },
+    query: {
+        join: {
+          screen: {
+            eager: true,
+          },
+        },
     },
 })
 @Controller('playlists')
