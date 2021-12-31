420d1d2be421590573be0735e8a92d2076d2cdf6
terminator_79 <aezz120813@gmail.com>
2021-12-31 15:58:40 +0600

    fix: migration

--- src/db/migrations/1640291168812-CreateFilesTable.ts
+++ src/db/migrations/1640291168812-CreateFilesTable.ts
@@ -3,19 +3,14 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class CreateFilesTable1640291168812 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('CREATE TABLE file (id SERIAL PRIMARY KEY, extension VARCHAR, url VARCHAR)');
-        await queryRunner.query('CREATE TABLE file_in_content (id SERIAL PRIMARY KEY, fileId INT, contentId INT, CONSTRAINT file_fk FOREIGN KEY (fileId) REFERENCES file(id), CONSTRAINT content_fk FOREIGN KEY (contentId) REFERENCES content(id))');
-        await queryRunner.query('ALTER TABLE content DROP COLUMN extension');
-        await queryRunner.query('ALTER TABLE content DROP COLUMN url');
+        await queryRunner.query('CREATE TABLE file (id SERIAL PRIMARY KEY, extension VARCHAR, url VARCHAR, contentId INT, CONSTRAINT content_fk FOREIGN KEY (contentId) REFERENCES content(id))');
+        await queryRunner.query('ALTER TABLE content DROP COLUMN extension, DROP COLUMN url');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('ALTER TABLE file_in_content DROP CONSTRAINT file_fk');
-        await queryRunner.query('ALTER TABLE file_in_content DROP CONSTRAINT content_fk');
-        await queryRunner.query('DROP TABLE file_in_content');
+        await queryRunner.query('ALTER TABLE file DROP CONSTRAINT content_fk');
         await queryRunner.query('DROP TABLE file');
-        await queryRunner.query('ALTER TABLE content ADD COLUMN extension VARCHAR');
-        await queryRunner.query('ALTER TABLE content ADD COLUMN url VARCHAR');
+        await queryRunner.query('ALTER TABLE content ADD COLUMN extension VARCHAR, ADD COLUMN url VARCHAR');
     }
 
 }
